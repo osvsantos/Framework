d7ad8a20cf70410f6ee6cfdf5d52dd28a1077936
osvsantos <osvsantos@gmail.com>
2025-01-23 00:05:04 -0300

    TestNG.xml and Parallel Testing

---
+++ src/test/java/testsFront/LoginTestLocalSuite.java
@@ -0,0 +1,74 @@
+package testsFront;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+import pages.LoginPage;
+import utils.config.ConfigurationManager;
+import utils.driver.DriverManager;
+import utils.driver.TargetFactory;
+import utils.listener.*;
+
+//selenium-java-lean-test-architecture-main\src\test\resources\suites
+
+@Listeners(TestListener.class) // Adiciona o listener para capturar falhas e tirar screenshots
+public class LoginTestLocalSuite {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @BeforeMethod
+    @Parameters("browser") // Recebe o parâmetro do navegador
+    public void setup(String browser) {
+        // Utilize TargetFactory para criar o driver com base no navegador
+        driver = TargetFactory.createInstance("local", browser);  // Passa o navegador selecionado
+        DriverManager.setDriver(driver); // Configura o driver no DriverManager
+
+        String url = ConfigurationManager.getProperty("url");
+        driver.get(url);
+
+        loginPage = new LoginPage(driver); // Agora instanciamos o LoginPage
+    }
+
+    @Test
+    public void testEmailBlankError() {
+        loginPage.login("", "secret_sauce");
+        boolean isEmailBlankErrorDisplayed = loginPage.isEmailBlankErrorDisplayed();
+        Assert.assertTrue(isEmailBlankErrorDisplayed, "Epic sadface: Username is required");
+    }
+
+    @Test
+    public void testPasswordBlankError() {
+        loginPage.login("standard_user", "");
+        boolean isPasswordBlankErrorDisplayed = loginPage.isPasswordBlankErrorDisplayed();
+        Assert.assertTrue(isPasswordBlankErrorDisplayed, "Epic sadface: Password is required");
+    }
+
+    @Test
+    public void testInvalidCredentialsError() {
+        loginPage.login("emailinvalido", "senhaerrada");
+        boolean isInvalidCredentialsErrorDisplayed = loginPage.isInvalidCredentialsErrorDisplayed();
+        Assert.assertTrue(isInvalidCredentialsErrorDisplayed, "Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void testValidLogin() {
+        loginPage.login("standard_user", "secret_sauce");
+        HomePage homePage = new HomePage(driver);
+
+        String expectedMessage = "Products";
+        String actualMessage = homePage.getAccessLevelMessage();
+        Assert.assertEquals(actualMessage, expectedMessage, "O texto da mensagem de nível de acesso está incorreto.");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        DriverManager.quitDriver();  // Garantir que o driver seja fechado corretamente
+    }
+}
